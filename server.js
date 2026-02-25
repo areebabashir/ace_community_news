1a310db1edb7583077298ef438836c5ef342a169
zainkazmi786 <zainkazmi258@gmail.com>
2026-02-25 22:01:41 +0500

    latest

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ const __dirname = path.dirname(__filename);
 const corsOptions = {
   origin: [
     'http://localhost:3000',
+    'http://127.0.0.1:8000',
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5176',
